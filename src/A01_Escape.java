5d9dd047512bc1b08ec8564718b2e703679af4c8
tak <ttak1997@daum.net>
2020-11-17 14:46:22 +0900

    My Second Commit

---
+++ src/A01_Escape.java
@@ -0,0 +1,54 @@
+
+public class A01_Escape {
+	
+	public static void main(String[] args) {
+		
+		
+		// Escape 문자
+		// - 특수한 기능을 가진 문자
+		// - 그냥 출력하려고 하면 문제가 생기는 문자들
+		// - 앞에 \가 붙은 문자는 Escape 문자로 간주함
+		// - 두 문자가 합쳐져 하나의 문자로 인식됨
+		
+		// \n : 줄바꿈의 기능을 가진 특수 문자 (linebreak, linefeed, newline ...)
+		System.out.println("안녕하세요!\n반갑습니다.");
+		
+		// \t : tab기능
+		System.out.println("안녕하세요!\t반갑습니다.");
+		System.out.println("이름\t혈액형\t최근헌혈여부");
+		System.out.println("홍길동\tA형\tX");
+		System.out.println("임꺽정\tO형\tO");
+		
+		// \\ : \를 출력하고싶을 때 사용
+		
+		// \" : "를 출력하고 싶을 때 사용
+		
+		// \' : '를 출력하고 싶을 때 사용
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
